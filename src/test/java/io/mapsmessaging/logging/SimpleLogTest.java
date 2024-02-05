9ed4612e82bfe5c3cd8a5e3328fc279a58be65c9
Matthew Buckton <matthew@buckton.org>
2024-02-05 11:08:03 +1100

    add tests for thread context

--- src/test/java/io/mapsmessaging/logging/SimpleLogTest.java
+++ src/test/java/io/mapsmessaging/logging/SimpleLogTest.java
@@ -16,7 +16,10 @@
  */
 package io.mapsmessaging.logging;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 public class SimpleLogTest {
 
@@ -34,6 +37,57 @@ public class SimpleLogTest {
   }
 
   @Test
+  void simpleExcessArgTest(){
+    Logger logger = LoggerFactory.getLogger(SimpleLogTest.class);
+    if(logger.isTraceEnabled())logger.log(LogMessages.TRACE, "Trace Message", "extra");
+    if(logger.isDebugEnabled())logger.log(LogMessages.DEBUG, "Debug Message", "extra");
+    if(logger.isInfoEnabled())logger.log(LogMessages.INFO, "Info Message", "extra");
+    if(logger.isWarnEnabled())logger.log(LogMessages.WARN, "Warn Message", "extra");
+    if(logger.isErrorEnabled())logger.log(LogMessages.ERROR, "Error Message", "extra");
+    if(logger.isFatalEnabled())logger.log(LogMessages.FATAL, "Fatal Message", "extra");
+    if(logger.isAuthEnabled())logger.log(LogMessages.AUTH, "Auth Message", "extra");
+    if(logger.isAuditEnabled())logger.log(LogMessages.AUDIT, "Audit Message", "extra");
+  }
+
+  @Test
+  void simpleThreadContextTest(){
+    ThreadContext.put("test", "value");
+    ThreadContext.put("test1", "value1");
+    Map<String, String> map = ThreadContext.getContext();
+    Assertions.assertNotNull(map);
+    Assertions.assertEquals("value", map.get("test"));
+    Assertions.assertEquals("value1", map.get("test1"));
+    ThreadContext.clearAll();
+    Assertions.assertNull(ThreadContext.getContext());
+
+    ThreadContext.put("test", "value");
+    ThreadContext.put("test1", "value1");
+    map = ThreadContext.getContext();
+    Assertions.assertNotNull(map);
+    Assertions.assertEquals("value", map.get("test"));
+    Assertions.assertEquals("value1", map.get("test1"));
+    ThreadContext.clear();
+    Assertions.assertNull(ThreadContext.getContext());
+
+    ThreadContext.put("test", "value");
+    ThreadContext.put("test1", "value1");
+    map = ThreadContext.getContext();
+    Assertions.assertNotNull(map);
+    Assertions.assertEquals("value", map.get("test"));
+    Assertions.assertEquals("value1", map.get("test1"));
+    ThreadContext.clearMap();
+    Assertions.assertNull(ThreadContext.getContext());
+
+    ThreadContext.putAll(map);
+    map = ThreadContext.getContext();
+    Assertions.assertNotNull(map);
+    Assertions.assertEquals("value", map.get("test"));
+    Assertions.assertEquals("value1", map.get("test1"));
+
+  }
+
+
+  @Test
   void simpleExceptionTest(){
     Exception ex = new Exception("Just a test");
     ex.fillInStackTrace();
@@ -46,6 +100,5 @@ public class SimpleLogTest {
     if(logger.isFatalEnabled())logger.log(LogMessages.FATAL, ex,"Fatal Message");
     if(logger.isAuthEnabled())logger.log(LogMessages.AUTH, ex,"Auth Message");
     if(logger.isAuditEnabled())logger.log(LogMessages.AUDIT, ex,"Audit Message");
-
   }
 }
